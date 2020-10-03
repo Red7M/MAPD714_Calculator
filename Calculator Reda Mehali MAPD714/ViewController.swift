6a1bcc8b3410e4fabac44b13c03af8eccce1e8b5
Redaesthetics <reda.mehali@gmail.com>
2020-10-02 19:02:54 -0700

    Initial Commit

---
+++ Calculator Reda Mehali MAPD714/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator Reda Mehali MAPD714
+//
+//  Created by Reda Mehali on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
